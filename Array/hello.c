d5195d39c56afb196f99a0582585b6edf98768cd
sunny08019 <1141621519@qq.com>
2020-01-10 23:51:38 +0800

    调试git

--- Array/hello.c
+++ Array/hello.c
@@ -3,11 +3,7 @@
 int main()
 {
     printf("hello world!\n");//增加注释
-<<<<<<< HEAD
-    getchar();
-=======
     getchar(); //获取输入
->>>>>>> 587f26b5210300163e82f67cf85b679ad1ecdbe1
     return 0;
     //system("pause");
 }
